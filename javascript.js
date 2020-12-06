a202d7bc4e1b0d5c7a9f43b08e51b634d15da79f
wasim <waseemnajjar625@gmail.com>
2020-12-06 21:32:31 +0300

    javascript header files added

--- javascript.js
+++ javascript.js
@@ -13,3 +13,37 @@ function toggleSearchInput() {
         input.placeholder = "";
     }
 }
+
+
+
+function waitPageLoading() {
+    setTimeout(function() {
+        const loadingPage = document.getElementById("loading-page");
+        const [container] = document.getElementsByClassName("container");
+        const header = document.getElementById("main-header").firstElementChild;
+
+        loadingPage.style.display = "none";
+        header.style.display = "flex";
+        container.style.display = "grid";
+
+        if (navigator.appVersion.indexOf("Chrome/") != -1) {
+            const profileName = document.getElementById("profile-name");
+            const postAuthorName = document.getElementById("post-author-name");
+
+            const profileSquare = document.getElementById("profile-square");
+
+            const elements = [profileName, postAuthorName];
+
+            console.log(elements);
+
+            elements.map((element) => {
+                element.classList.add("letter-spacing");
+            });
+
+            profileSquare.classList.add("font-16");
+        }
+    }, 2000);
+}
+
+
+waitPageLoading();
