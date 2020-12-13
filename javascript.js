d6d1e709beb1f14fa8f8f6f8a0fd7128e8795c3f
wasim <waseemnajjar625@gmail.com>
2020-12-13 13:39:01 +0300

    Updating

--- javascript.js
+++ javascript.js
@@ -14,7 +14,48 @@ function toggleSearchInput() {
     }
 }
 
+function showProfileGroupIcon(element) {
+    const [icon] = element.querySelectorAll("span.fas");
 
+    if (icon.style.display === "block") {
+        icon.style.display = "none";
+    } else {
+        icon.style.display = "block";
+    }
+}
+
+function toggleProfileGroupList(icon) {
+    const list = icon.parentElement.nextElementSibling;
+    const link = list.nextElementSibling;
+
+    if (icon.className === "fas fa-angle-up") {
+        icon.className = "fas fa-angle-down";
+        list.style.display = "none";
+        if (link) {
+            link.style.display = "none";
+        }
+    } else {
+        icon.className = "fas fa-angle-up";
+        list.style.display = "block";
+        if (link) {
+            link.style.display = "block";
+        }
+    }
+}
+
+function showRecommendationsInfo(element) {
+    const [div, span, square] = element.children;
+
+    if (div.style.display === "block") {
+        div.style.display = "none";
+        span.style.display = "none";
+        square.style.display = "none";
+    } else {
+        div.style.display = "block";
+        span.style.display = "block";
+        square.style.display = "block";
+    }
+}
 
 function waitPageLoading() {
     setTimeout(function() {
@@ -45,5 +86,20 @@ function waitPageLoading() {
     }, 2000);
 }
 
+window.addEventListener("scroll", (e) => {
+    const profileGroup = document.getElementById("profile-groups");
+    const linkedinSection = document.getElementById("linkedin-section");
+    const rightAside = document.getElementById("right-aside");
+
+    if (window.scrollY > 356) {
+        profileGroup.classList.add("position-fixed");
+        linkedinSection.classList.add("position-fixed", "right-aside-fixed");
+        rightAside.classList.add("vanish");
+    } else {
+        profileGroup.classList.remove("position-fixed");
+        linkedinSection.classList.remove("position-fixed", "right-aside-fixed");
+        rightAside.classList.remove("vanish");
+    }
+});
 
 waitPageLoading();
